f035518437e3c1b5052b7003ed2e23f1933e59d7
Douglas Oliveira <douglas.olliveira1997@gmail.com>
2023-11-10 21:20:14 -0300

    Projeto Finalizado

---
+++ idade.js
@@ -0,0 +1,35 @@
+const aviso = document.getElementById('aviso');
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click',()=>{
+    const id = document.getElementById('idade');
+   let idade = parseInt(id.value);
+   
+   aviso.innerHTML = "";
+
+
+if (idade >= 18 && idade <= 69){
+    aviso.style = " color: #2980b9;"
+    aviso.innerHTML= 'O voto é obrigatório' ;
+    
+} 
+else if(idade >= 16 && idade <= 17  ){
+    aviso.style = " color: #2980b9;"
+    aviso.innerHTML= "Você é elegível para votar, porém não é obrigatório.";
+   
+} else if(idade >= 70 && idade <= 100 ){
+    aviso.style = " color: #2980b9;"
+    aviso.innerHTML= 'Seu voto não é obrigatório.';
+    
+}else if(idade<= 15 && idade >= 1 ){
+    aviso.style = "color:red;"
+    aviso.innerHTML= "Você é menor de idade, Não e elegível";
+    
+} else{
+    aviso.style = "color:red;"
+    aviso.innerHTML = "Adicione uma idade Verdadeira!"
+}
+})
+
+
